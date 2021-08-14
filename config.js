88260d0c572aadf508b95a69372dfb6356420fb9
1MishraBhavesh <74255631+1MishraBhavesh@users.noreply.github.com>
2021-08-14 18:57:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import  firebase from 'firebase'
+require('@firebase/firestore')
+
+// For Firebase JS SDK v7.20.0 and later, measurement
+export const  firebaseConfig = {
+    apiKey: "AIzaSyA-iVZ2IHvGrQ_VEgikOv0OwH7rGrQt6fY",
+    authDomain: "storytellingapp-68c99.firebaseapp.com",
+    databaseURL: "https://storytellingapp-68c99-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-68c99",
+    storageBucket: "storytellingapp-68c99.appspot.com",
+    messagingSenderId: "203849951269",
+    appId: "1:203849951269:web:3f6b02f228e672cac777cc"
+  };
+  // Initialize Firebase
+
+
